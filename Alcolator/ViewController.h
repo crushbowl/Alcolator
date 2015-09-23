687bc654041ac6c75dafecf7d6842860d83623d5
JanL <janelle.h.writes@gmail.com>
2015-09-23 14:03:22 -0400

    Update Alcoholator

--- Alcolator/ViewController.h
+++ Alcolator/ViewController.h
@@ -11,5 +11,15 @@
 @interface ViewController : UIViewController
 
 
+@property (weak, nonatomic) IBOutlet UITextField *beerPercentTextField;
+
+@property (weak, nonatomic) IBOutlet UISlider *beerCountSlider;
+
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+
+- (void)buttonPressed:(UIButton *) sender;
+
+
 @end
 
